ac04c5067fc1704dfadaa074c2944ba07268d095
Edgar López <edgar.lr.18@gmail.com>
2021-04-15 18:35:11 -0500

    Fix usefavs  bug

--- components/FavsProvider.tsx
+++ components/FavsProvider.tsx
@@ -1,26 +1,52 @@
 import { FavsContext } from 'lib/hooks/use-favs'
-import { useLocalStorage } from 'lib/hooks/use-local-storage'
-import { useCallback } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import { useSession } from 'next-auth/client'
 
 const FavsProvider = ({ children }: { children: React.ReactNode }) => {
   const [session] = useSession()
+  const [favs, setFavs] = useState([])
 
-  const [favs, setFavs] = useLocalStorage<TCharacter[]>(
-    `favs-${session?.user.email}`,
-    []
-  )
+  const readValue = () => {
+    if (typeof window === undefined) []
+    try {
+      const item = window.localStorage.getItem(`favs-${session?.user.email}`)
+      return item ? JSON.parse(item) : []
+    } catch (e) {
+      return []
+    }
+  }
+
+  useEffect(() => {
+    setFavs(readValue())
+  }, [])
+
+  const setLocalStorage = (value: any) => {
+    if (typeof window === undefined) return
+
+    try {
+      window.localStorage.setItem(
+        `favs-${session?.user.email}`,
+        JSON.stringify(value)
+      )
+      setFavs(value)
+      window.dispatchEvent(new Event('local-storage'))
+    } catch (e) {
+      return
+    }
+  }
 
   const addToFavs = useCallback(
     (character: TCharacter) => {
-      setFavs([...favs, character])
+      setLocalStorage([...favs, character])
     },
     [favs, setFavs]
   )
 
   const removeFromFavs = useCallback(
     (character: TCharacter) => {
-      setFavs(favs.filter((item: TCharacter) => item.url !== character.url))
+      setLocalStorage(
+        favs.filter((item: TCharacter) => item.url !== character.url)
+      )
     },
     [favs, setFavs]
   )
