405490fac6766fe71fd4f8e0d26f90b5cfd1a9ab
Edgar López <edgar.lr.18@gmail.com>
2021-04-15 17:31:25 -0500

    Fix auth bug

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -7,11 +7,8 @@ export default NextAuth({
     Providers.Google({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      authorizationUrl:
+        'https://accounts.google.com/o/oauth2/v2/auth?prompt=consent&access_type=offline&response_type=code',
     }),
   ],
-  callbacks: {
-    async redirect(url, baseUrl) {
-      return baseUrl
-    },
-  },
 })
