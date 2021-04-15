fb0fd86f4ffbc6c69f123b4866438044cd47774f
Edgar López <edgar.lr.18@gmail.com>
2021-04-15 15:36:19 -0500

    Add useLocalStorage

--- pages/login.tsx
+++ pages/login.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 
 function LoginPage() {
   return (
-    <Layout>
-      <section className="w-96 h-72 rounded-2xl mx-auto border py-12 px-8 flex flex-col">
+    <Layout header={false}>
+      <section className="w-96 h-72 rounded-2xl mx-auto mt-16 border py-12 px-8 flex flex-col">
         <h2 className="mx-auto mt-4 font-bold text-gray-600">Login</h2>
         <p className="text-sm text-center text-gray-800">
           Login using your google account
